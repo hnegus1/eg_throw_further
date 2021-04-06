ec8c4d13663751abd72079516552b265c2dc3878
Harry <harrynegus@hotmail.co.uk>
2021-04-06 17:49:12 +0100

    Added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,18 @@ module.exports = {
   siteMetadata: {
     title: "When Did EG Last Throw",
   },
-  plugins: ['gatsby-plugin-postcss'],
+  plugins: [
+    'gatsby-plugin-postcss',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+          trackingId: process.env.GOOGLE_ANALYTICS_ID,
+          head: true,
+          anonymize: true,
+          respectDNT: true,
+          pageTransitionDelay: 0,
+          defer: false
+      }
+    }
+  ],
 };
